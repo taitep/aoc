bcf0a0e22871b002b2a662078dffa2cd9397aefc
taitep <taitep@taitep.se>
2024-12-01 11:44:13 +0100

    Update python input parsing template to allow being run in isolation.

--- templates/python/parse.py
+++ templates/python/parse.py
@@ -1,8 +1,15 @@
+def parse_input(text: str):
+    pass
+
+
 def parse_file(filename: str):
     with open(filename, "r") as f:
         contents = f.readall()
     return parse_input(contents)
 
 
-def parse_input(text: str):
-    pass
+def main():
+    from pprint import pprint
+    from sys import argv
+
+    pprint(parse_file(argv[1]))

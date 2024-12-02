737bf9755fdea53dc7c3ccd61fdaf6597342175a
taitep <taitep@taitep.se>
2024-12-02 13:16:46 +0100

    2024d2: solve part 1

--- 2024/day02/src/bin/part1.rs
+++ 2024/day02/src/bin/part1.rs
@@ -16,5 +16,45 @@ fn main() -> Result<()> {
 }
 
 fn solve(input: &str) -> Result<i32> {
-    todo!()
+    let lines = input.lines().map(|x| {
+        x.split(" ")
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>()
+    });
+
+    Ok(lines
+        .filter(|line| {
+            let mut line = line.iter();
+
+            let mut previous = *line.next().unwrap();
+            let mut current = *line.next().unwrap();
+
+            let upper_bound: i32;
+            let lower_bound: i32;
+            if previous > current {
+                upper_bound = 3;
+                lower_bound = 1;
+            } else if previous < current {
+                upper_bound = -1;
+                lower_bound = -3;
+            } else {
+                return false;
+            }
+
+            loop {
+                let difference = previous - current;
+                if difference < lower_bound || difference > upper_bound {
+                    return false;
+                }
+
+                previous = current;
+
+                if let Some(next) = line.next() {
+                    current = *next;
+                } else {
+                    return true;
+                }
+            }
+        })
+        .count() as i32)
 }

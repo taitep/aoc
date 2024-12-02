e0e9c2cb351a0becae4184a95a36a3615f3c108c
taitep <taitep@taitep.se>
2024-12-02 12:42:52 +0100

    Write boilerplate for day 2 part 1

--- 2024/day02/src/bin/part1.rs
+++ 2024/day02/src/bin/part1.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use anyhow::{bail, Result};
+use std::{env::args, fs::read_to_string};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        bail!("Wrong amount of arguments, expected filename for input file.")
+    }
+
+    let filename = &args[1];
+    let input = read_to_string(filename)?;
+
+    println!("{}", solve(&input)?);
+
+    Ok(())
+}
+
+fn solve(input: &str) -> Result<i32> {
+    todo!()
+}
